091e18e39d8dfcaecde8506f6e603944d4819154
Fahad Khan <fahad.khan@wbd.com>
2025-05-16 19:25:24 +0530

    Fix path

--- signed_auto_commit.py
+++ signed_auto_commit.py
@@ -7,7 +7,7 @@ print(os.getcwd())
 # --- Configuration ---
 GITHUB_TOKEN = sys.argv[1]  # Your GitHub PAT
 REPO_NAME = "fahadk-wbd/gh-issue-template-poc" # Format: username/repo
-LOCAL_FILE_PATH = "gh-issue-template-poc/issue_template.md" 
+LOCAL_FILE_PATH = "issue_template.md" 
 REPO_FILE_PATH = "gh-issue-template-poc/issue_template.md" 
 BRANCH = "new"
 COMMIT_MESSAGE = "This is commit message"
